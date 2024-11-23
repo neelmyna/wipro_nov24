0fc7e59366b9675217c71caae067c36985496263
neelmyna <nithincheeta@gmail.com>
2024-11-23 13:16:04 +0530

    day3 session2 notes updated

--- day3_day4/p28.c
+++ day3_day4/p28.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
-Read N
-SMALL_DIGIT = 9
-BIG_DIGIT = 0
-TEMP_NUMBER = N
-until N is not 0 do:
-    REMAINDER_DIGIT = N % 10
-    N = N / 10
-    if SMALL_DIGIT > REMAINDER_DIGIT then:
-        SMALL_DIGIT = REMAINDER_DIGIT
-    if BIG_DIGIT < REMAINDER_DIGIT then:
-        BIG_DIGIT = REMAINDER_DIGIT
-print SMALL_DIGIT
-print BIG_DIGIT
-*/
+
 int main(int argCount, char *args[])
 {
-    int input_number = atoi(args[1]);
-    printf("User given number is %d \n", input_number);
-    int small_digit = 9, big_digit = 0, remainder_digit = 0;
-    while (input_number != 0)
+    int inputNumber = atoi(args[1]);
+    int sumOfDigits1 = 0, sumOfDigits2 = 0, flip = 0, remainderDigit = 0;
+    printf("User given number is %d \n", inputNumber);
+    while (inputNumber != 0)
     {
-        remainder_digit = input_number % 10;
-        if (small_digit > remainder_digit)
-            small_digit = remainder_digit;
-        if (big_digit < remainder_digit)
-            big_digit = remainder_digit;
-        input_number = input_number / 10;
+        remainderDigit = inputNumber % 10;
+        inputNumber = inputNumber / 10;
+        if (flip == 0)
+        {
+            sumOfDigits1 += remainderDigit;
+            flip = 1;
+        }
+        else
+        {
+            sumOfDigits2 += remainderDigit;
+            flip = 0;
+        }
     }
-    printf("Smallest digit = %d, Biggest Digit = %d", small_digit, big_digit);
+    if (flip == 0)
+        printf("Sum of even placed digits is %d", sumOfDigits1);
+    else
+        printf("Sum of even placed digits is %d", sumOfDigits2);
 }
