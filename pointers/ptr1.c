31dc8a4eddeb4f19d9c4551e67711c7e9394803c
neelmyna <nithincheeta@gmail.com>
2024-11-26 12:34:20 +0530

    pointer programs are added

---
+++ pointers/ptr1.c
@@ -0,0 +1,15 @@
+// To understand the characteristic of a number
+#include <stdio.h>
+
+int main()
+{
+    int num = 20;
+    printf("%d  %d  %d  %d", num - 1, num, num + 1, num + 2);
+}
+/*
+num is 20
+num - 1 is 19
+Thus 19 is previous number of 20
+num+1 is 21 which is next number of 20
+num_10 is 10th number from 20
+*/
